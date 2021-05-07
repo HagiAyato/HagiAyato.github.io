a7924f98d5c856fe87518adda54fb1a3c801c70b
HagiAyato <h8a9g11i.@gmail.com>
2021-05-07 20:38:07 +0900

    変換部分の機能拡張

--- VowelJP/script/VowelJP.js
+++ VowelJP/script/VowelJP.js
@@ -20,6 +20,11 @@ const jpToVovel = {
     'ぱ': 'あ', 'ぴ': 'い', 'ぷ': 'う', 'ぺ': 'え', 'ぽ': 'お'
 }
 
+const smallToLarge = {
+    'ぁ': 'あ', 'ぃ': 'い', 'ぅ': 'う', 'ぇ': 'え', 'ぉ': 'お',
+    'ゃ': 'あ', 'ゅ': 'う', 'ょ': 'お', 'ゎ': 'あ'
+}
+
 /**
  * 日本語平仮名⇒母音
  */
@@ -28,18 +33,39 @@ function convertToVowel() {
     const jp = $('#input').val();
     // 変換後の文字
     let vowel = jp;
-    
+
     // と/ど+'ぅ'をうに変換
-    // て/で+ぃ/ゅをい/うに変換
-    vowel = vowel.replace(/[とど]ぅ/g, "う").replace(/[てで]ぃ/g, "い").replace(/[てで]ゅ/g, "う");
+    vowel = vowel.replace(/[とど]ぅ/g, "う")
+        // て/で+ぃ/ゅをい/うに変換
+        .replace(/[てで]ぃ/g, "い").replace(/[てで]ゅ/g, "う");
 
     // 大文字+ぅを変換
     let convertedArray = new Array();
-    for(let index in vowel.split('')){
-        convertedArray.push(jpToVovel[vowel[index]]);
+    for (let index in vowel.split('')) {
+        let c = jpToVovel[vowel[index]];
+        convertedArray.push(c != undefined ? c : vowel[index]);
     }
     vowel = convertedArray.join('');
 
+    // うーをううに変換
+    vowel = vowel.replace(/うー/g, "うう")
+        // う+ゎ/ぁ/ぃ/ぇ/ぉを母音に変換
+        .replace(/うゎ/g, "うあ").replace(/うぁ/g, "うあ").replace(/うぃ/g, "うい").replace(/うぇ/g, "うぇ").replace(/うぉ/g, "うお")
+        // いー/ぃーをいい/ぃいに変換
+        .replace(/いー/g, "いい").replace(/ぃー/g, "ぃい")
+        // い/ぃ+ゃ/ゅ/ぇ/ょを母音に変換
+        .replace(/[いぃ]ゃ/g, "あ").replace(/[いぃ]ゅ/g, "う").replace(/[いぃ]ぇ/g, "え").replace(/[いぃ]ょ/g, "お")
+
+    // 小さい文字を大きい文字に変換
+    convertedArray = new Array();
+    for (let index in vowel.split('')) {
+        let c = smallToLarge[vowel[index]];
+        convertedArray.push(c != undefined ? c : vowel[index]);
+    }
+    vowel = convertedArray.join('');
+
+    // 伸ばし棒対応
+    vowel = vowel.replace(/あー/g, "ああ").replace(/いー/g, "いい").replace(/うー/g, "うう").replace(/えー/g, "ええ").replace(/おー/g, "おお")
     // 変換後の文字を出力
     $('#output').val(vowel);
 }
