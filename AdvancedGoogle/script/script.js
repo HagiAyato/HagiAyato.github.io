4c523a99211f97609a128a9be640a98b59dc30a8
HagiAyato <h8a9g11i.@gmail.com>
2021-05-30 16:38:00 +0900

    下記機能追加 日付指定 サイト指定

--- AdvancedGoogle/script/script.js
+++ AdvancedGoogle/script/script.js
@@ -26,6 +26,14 @@ window.onload = function () {
     // Object.keys(urls).forEach(function (key) {
     //     $("#site").append("<option value='" + key + "'>" + key + "</option>");
     // });
+
+    // 日付入力
+    const date = new Date();
+    const formattedDate = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2)
+    $("#before").val(formattedDate);
+    $("#before").prop("disabled", true);
+    $("#after").val(formattedDate);
+    $("#after").prop("disabled", true);
 }
 
 /**
@@ -53,12 +61,36 @@ function addQuery(query) {
 }
 
 /**
+ * チェックボックスと対応する要素の有効無効切替
+ * @param {string} target 
+ * @param {boolean} isChecked 
+ */
+function changeEnable(target, isChecked) {
+    $("#" + target).prop("disabled", !isChecked);
+}
+
+/**
  * 検索処理本体
  */
 function search() {
     const word = $("#word").val();
     // 検索ワード空欄の場合は処理中止
     if (word.length < 1) return;
+    let query = urls["Google"] + word;
+    // 日付指定
+    const before = $("#before").val();
+    if (0 < before.length && $("#chk_before").prop("checked")){
+        query += (" before:" + before);
+    }
+    const after = $("#after").val();
+    if (0 < after.length && $("#chk_after").prop("checked")){
+        query += (" after:" + after);
+    }
+    // テキスト指定
+    const url = $("#site").val();
+    if (0 < url.length && $("#chk_site").prop("checked")){
+        query += (" site:" + url);
+    }
     // 新しいタブを開き、ページを表示
-    window.open(urls["Google"] + word, "_blank");
+    window.open(query, "_blank");
 }
